23093f2f6a9cc9a6f896878a2043a56e069254c0
Daniel Kelly <me@danielkelly.io>
2022-12-15 16:32:25 -0600

    * add Prettier for formatting * add ESLint for linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  extends: ["eslint:recommended", "@nuxt/eslint-config", "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
--- app.vue
+++ app.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="h-[100vh] bg-gray-900 flex justify-center items-center text-white">
+  <div
+    class="h-[100vh] bg-gray-900 flex justify-center items-center text-white"
+  >
     <div>
       <h1 class="text-3xl">Hello Nuxt Base</h1>
-    <ul>
-      <li>Nuxt 3</li>
-      <li>VueUse</li>
-      <li>Tailwind CSS</li>
-    </ul>
+      <ul>
+        <li>Nuxt 3</li>
+        <li>VueUse</li>
+        <li>Tailwind CSS</li>
+      </ul>
     </div>
-    
   </div>
 </template>
