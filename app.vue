75b00502be1f240e290ce9b511b15d20bc9ae7aa
Daniel Kelly <me@danielkelly.io>
2022-12-15 16:13:49 -0600

    *Install VueUse * Install Tailwind CSS

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="h-[100vh] bg-gray-900 flex justify-center items-center text-white">
+    <div>
+      <h1 class="text-3xl">Hello Nuxt Base</h1>
+    <ul>
+      <li>Nuxt 3</li>
+      <li>VueUse</li>
+      <li>Tailwind CSS</li>
+    </ul>
+    </div>
+    
   </div>
 </template>
